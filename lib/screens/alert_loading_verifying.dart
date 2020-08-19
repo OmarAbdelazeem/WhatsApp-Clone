915c27b60173677f19fea6afc4efa577b56c2645
OmarAbdelazeem <44911566+OmarAbdelazeem@users.noreply.github.com>
2020-08-19 20:51:16 +0200

    Update alert_loading_verifying.dart

--- lib/screens/alert_loading_verifying.dart
+++ lib/screens/alert_loading_verifying.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../providers/data.dart';
+import '../providers/auth_data.dart';
 
 class AlertLoadingVerifying extends StatefulWidget {
   final String smsCode;
