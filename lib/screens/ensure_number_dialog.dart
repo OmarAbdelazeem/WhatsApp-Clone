ed79f03697f1b75f02469638e409833d634325fa
OmarAbdelazeem <44911566+OmarAbdelazeem@users.noreply.github.com>
2020-08-19 20:51:59 +0200

    Update ensure_number_dialog.dart

--- lib/screens/ensure_number_dialog.dart
+++ lib/screens/ensure_number_dialog.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:whatsapptest/providers/data.dart';
+import '../providers/auth_data.dart';
 
 class EnsureNumberDialog extends StatelessWidget {
   @override
